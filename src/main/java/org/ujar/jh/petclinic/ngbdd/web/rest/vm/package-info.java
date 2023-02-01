50833a33a791b11dc9372a0776e69b8b4154bcea
Dima Denysenko <coding@dimdnk.com>
2023-02-01 15:41:30 +0200

    Initial version of petclinic generated by generator-jhipster@7.9.3

---
+++ src/main/java/org/ujar/jh/petclinic/ngbdd/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package org.ujar.jh.petclinic.ngbdd.web.rest.vm;
---
+++ src/test/java/org/ujar/jh/petclinic/ngbdd/cucumber/CucumberIT.java
@@ -0,0 +1,8 @@
+package org.ujar.jh.petclinic.ngbdd.cucumber;
+
+import io.cucumber.junit.platform.engine.Cucumber;
+import org.ujar.jh.petclinic.ngbdd.IntegrationTest;
+
+@Cucumber
+@IntegrationTest
+class CucumberIT {}
