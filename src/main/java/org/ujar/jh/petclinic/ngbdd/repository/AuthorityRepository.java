993d75afb73e5c301255fa90a8f094977eb0f378
Dima Denysenko <coding@dimdnk.com>
2023-12-14 16:30:18 +0200

    chore: migrate to dev.knowhowto group

--- src/main/java/org/ujar/jh/petclinic/ngbdd/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package org.ujar.jh.petclinic.ngbdd.repository;
-
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
-import org.ujar.jh.petclinic.ngbdd.domain.Authority;
-
-/**
- * Spring Data R2DBC repository for the {@link Authority} entity.
- */
-public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
